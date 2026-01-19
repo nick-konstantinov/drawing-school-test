85730056dab118b97df17d5184c79c1715d7160e
Nick Konstantinov <nick.konstantinov.job@gmail.com>
2026-01-20 01:00:45 +0400

    feat: add tariffs

--- js/main.js
+++ js/main.js
@@ -21,3 +21,48 @@ $(document).ready(function() {
     });
   });
 });
+
+//Сurrency Еxchange
+$(function () {
+  const RATE = 70;
+
+  function formatPrice(value) {
+    return value.toLocaleString('ru-RU');
+  }
+
+  function setRub($card) {
+    const oldPrice = Number($card.data('old'));
+    const newPrice = Number($card.data('new'));
+
+    $card.find('.tariff-old-price').text(formatPrice(oldPrice));
+    $card.find('.tariff-new-price').text(formatPrice(newPrice));
+    $card.find('.tariff-currency').text('₽');
+
+    $card.find('.is-rub').addClass('is-active');
+    $card.find('.is-usd').removeClass('is-active');
+  }
+
+  function setUsd($card) {
+    const oldPrice = Math.round(Number($card.data('old')) / RATE);
+    const newPrice = Math.round(Number($card.data('new')) / RATE);
+
+    $card.find('.tariff-old-price').text(formatPrice(oldPrice));
+    $card.find('.tariff-new-price').text(formatPrice(newPrice));
+    $card.find('.tariff-currency').text('$');
+
+    $card.find('.is-usd').addClass('is-active');
+    $card.find('.is-rub').removeClass('is-active');
+  }
+
+  $('.tariffs-card').each(function () {
+    setRub($(this));
+  });
+
+  $(document).on('click', '.is-rub', function () {
+    setRub($(this).closest('.tariffs-card'));
+  });
+
+  $(document).on('click', '.is-usd', function () {
+    setUsd($(this).closest('.tariffs-card'));
+  });
+});
